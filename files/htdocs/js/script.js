c047d7d1b1451ebf6c2ec21d3306cbf3e12e6699
HiGarfield <HiGarfield@126.com>
2022-02-19 16:48:11 +0800

    fix vlan prot display

--- files/htdocs/js/script.js
+++ files/htdocs/js/script.js
@@ -201,12 +201,15 @@
         that.after("<span class='panel-title'>" + that.text() + "</span>");
     });
 
+    /*fix vlan prot*/
+    /********************************************
     $(".cbi-section-table-titles, .cbi-section-table-descr, .cbi-section-descr").each(function () {
         var that = $(this);
         if (that.text().trim() == ""){
             that.css("display", "none");
         }
     });
+    ********************************************/
 
 
     $(".main-right").focus();
